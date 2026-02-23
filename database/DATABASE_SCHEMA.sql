a670830e2770661d01d89749159d56ea267aa7d1
Angebright <a.muhawenim@alustudent.com>
2026-02-22 22:37:10 -0800

    location's table

--- database/DATABASE_SCHEMA.sql
+++ database/DATABASE_SCHEMA.sql
@@ -12,3 +12,17 @@ CREATE TABLE zone (
     PRIMARY KEY (zone_id)
 ) ENGINE=InnoDB;
 
+
+-- 2. Location (50 rows)
+-- Each pickup or dropoff spot, linked to a zone
+CREATE TABLE location (
+    loc_id        INT            NOT NULL,
+    borough       VARCHAR(50)    DEFAULT NULL,
+    zone_name     VARCHAR(100)   DEFAULT NULL,
+    service_zone  VARCHAR(50)    DEFAULT NULL,
+    zone_id       INT            DEFAULT NULL,
+    PRIMARY KEY (loc_id)
+) ENGINE=InnoDB;
+
+
+
